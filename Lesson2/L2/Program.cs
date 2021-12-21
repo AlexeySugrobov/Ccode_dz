3381e31c1ac0e6d6eb3b86671622aa343b36f152
AlexeySugrobov <81681398+AlexeySugrobov@users.noreply.github.com>
2021-12-21 17:52:30 +0300

    Добавил дз2

---
+++ Lesson2/L2/Program.cs
@@ -0,0 +1,6 @@
+﻿//Сугробов 2. Написать метод подсчета количества цифр числа.
+
+int value = 1233445;
+string abc = Convert.ToString(value);
+int num_val = abc.Length;
+Console.WriteLine(num_val);
---
+++ Lesson2/L3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Сугробов 3. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+
+Console.Write("Введите число, ноль прекратит ввод: ");
+int sum = 0;
+int num = 0; 
+do
+{
+    num = int.Parse(Console.ReadLine() ?? ""); // string.Empty
+    if (num > 0 && num % 2 == 1)
+    {
+        sum += num; //sum = sum + num
+    }
+} while (num != 0);
+ 
+Console.Write("Sum: " + sum);
